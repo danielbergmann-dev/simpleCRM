e5a512513c90105e9e19b716c79f2c4a9318dedd
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-06-27 15:08:04 +0200

    update User DB

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {
-  constructor(public dialog: MatDialog) {}
+export class UserComponent implements OnInit {
+  allUsers: any = [];
+
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        console.log('received changes from debugger', changes);
+        this.allUsers = changes;
+      });
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
