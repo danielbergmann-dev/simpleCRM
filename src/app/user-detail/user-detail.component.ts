65eb9c5bc7db349e46e3e157e19ff28277a2449b
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-06-28 22:09:02 +0200

    not ready user id from firestore

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent implements OnInit {
   userId = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((paramMap) => {
