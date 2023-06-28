7d5d107c35ffe82711760094b4eeeaba254f0ee9
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-06-28 21:30:35 +0200

    add user ID

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  userId = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      this.userId = paramMap.get('id') || '';
+      console.log('user ID', this.userId);
+      
+    });
+  }
 }
