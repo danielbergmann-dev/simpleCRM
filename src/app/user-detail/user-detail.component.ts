1426f6b50033f6705156ec851a2f94bf790d1e8d
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-07-01 11:24:02 +0200

    add user objekt from JSON

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,6 +10,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class UserDetailComponent implements OnInit {
   userId = '';
+  user: User = new User();
+
 
   constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {}
 
@@ -16,7 +19,23 @@ export class UserDetailComponent implements OnInit {
     this.route.paramMap.subscribe((paramMap) => {
       this.userId = paramMap.get('id') || '';
       console.log('user ID', this.userId);
+      this.getUser();
       
     });
   }
+
+  getUser() {
+    this.firestore
+      .collection('users')
+      .doc(this.userId)
+      .valueChanges()
+      .subscribe((user: any) => {
+        console.log('user', user);
+        this.user = new User(user);
+        
+      });
+  }
+
+
+
 }
