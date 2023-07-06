7a4f8781aea2479d156a48ee020006bbbb44f90c
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-07-06 13:06:33 +0200

    edit weather

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,23 +8,24 @@ import { WetterService } from '../wetter.service';
 })
 export class DashboardComponent {
   wetterDaten: any;
+  vorschauDaten: any[] = [];
 
   constructor(private wetterService: WetterService) { }
 
   getWetter(stadt: string) {
     this.wetterService.getWetter(stadt)
-    .subscribe(
-      data => { this.wetterDaten = data; },
-      err => console.error(err),
-      () => console.log('Wetterdaten abgerufen')
-    );
+    .subscribe({
+      next: (data: any) => {
+        this.wetterDaten = data;
+        console.log(data);
+        if(data.list) {
+          this.vorschauDaten = data.list.slice(0, 24);
+        } else {
+          console.error('Keine "list" Eigenschaft im Antwortobjekt');
+        }
+      },
+      error: err => console.error('Fehler beim Abrufen der Wetterdaten:', err),
+      complete: () => console.log('Wetterdaten abgerufen')
+    });
   }
 }
-
-
-
-
-
-
-
-
