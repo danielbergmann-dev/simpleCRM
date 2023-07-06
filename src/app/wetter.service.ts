ed612fdd192e51587aa72dabdca98c9c2698d853
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-07-06 13:41:51 +0200

    fix weather service

--- src/app/wetter.service.ts
+++ src/app/wetter.service.ts
@@ -1,39 +1,4 @@
-/* import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class WetterService {
-
-  apiKey = '7943953881a940528b678aa00a5cafd4';
-  apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-
-  constructor(private http: HttpClient) { }
-
-  getWetter(stadt: string) {
-    return this.http.get(this.apiUrl + stadt + '&appid=' + this.apiKey);
-  }
-}
- */
-/* import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class WetterService {
-
-  apiKey = '7943953881a940528b678aa00a5cafd4';
-  apiUrl = 'https://api.openweathermap.org/data/2.5/weather?';
-
-  constructor(private http: HttpClient) { }
-
-  getWetter(stadt: string) {
-    return this.http.get(this.apiUrl + 'q=' + stadt + '&units=metric&appid=' + this.apiKey);
-  }
-}
- */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
