5ef7cf48ccd0b427e0eda4f7c1255ee78c3d1d73
Daniel Bergmann <dhetzer3@gmail.com>
2023-07-29 23:19:36 +0200

    fix clean login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Subscription } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -6,10 +8,24 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'simple-crm';
+  isLoggedIn!: boolean;
+  private subscription!: Subscription;
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private authService: AuthService) {
   
+}
+
+ngOnInit() {
+  this.subscription = this.authService.isLoggedIn.subscribe(
+    (loggedIn) => {
+      this.isLoggedIn = loggedIn;  // Update login status
+    }
+  );
+}
+
+ngOnDestroy() {
+  this.subscription.unsubscribe();  // Unsubscribe when the component is destroyed
 }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,22 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private loggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth) {}
+
+  get isLoggedIn() {
+    return this.loggedIn.asObservable();  // Provide an Observable for the login status
+  }
+
+  logout() {
+    return this.afAuth.signOut().then(() => {
+      this.loggedIn.next(false);  // Set login status to false
+    });
+  }
+  
 
   signup(email: string, password: string) {
     return this.afAuth.createUserWithEmailAndPassword(email, password);
   }
 
   login(email: string, password: string) {
-    return this.afAuth.signInWithEmailAndPassword(email, password);
+    return this.afAuth.signInWithEmailAndPassword(email, password).then(() => {
+      this.loggedIn.next(true);
+    });
   }
 
   guestLogin() {
-    return this.afAuth.signInAnonymously();
+    return this.afAuth.signInAnonymously().then(() => {
+      this.loggedIn.next(true);
+    });
   }
 }
